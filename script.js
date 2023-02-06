1c726e92201ae00a1591b16378e3608426b58a0a
jonesform <lee@jonesforn.com>
2023-02-06 10:41:51 +0000

    menus corrected for now

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const contentsContainer = document.querySelector('.contents');
+
 fetch('contents.json')
   .then(response => response.json())
   .then(data => {
-    const menu = document.createElement('ul');
-    menu.classList.add('content-menu');
-    document.body.appendChild(menu);
-
     for (const content of data.contents) {
-      const menuItem = document.createElement('li');
-      menuItem.textContent = content.title;
-      menuItem.addEventListener('click', () => {
-        document.querySelector(`[data-id="${content.id}"]`).scrollIntoView();
-      });
-      menu.appendChild(menuItem);
-
       const contentEl = document.createElement('div');
       contentEl.classList.add('entry-container');
       contentEl.setAttribute('data-id', content.id);
@@ -38,23 +28,6 @@ fetch('contents.json')
       contentsContainer.appendChild(contentEl);
     }
   });
-
-// Get the contents menu item and contents menu container
-const contentsMenuItem = document.querySelector("#contents-menu-item");
-const contentsMenu = document.querySelector("#contents-menu");
-
-// When the contents menu item is clicked, toggle the visibility of the contents menu container
-contentsMenuItem.addEventListener("click", function() {
-  contentsMenu.classList.toggle("show");
-});
-
-// When clicking outside the contents menu container, hide the contents menu container
-window.addEventListener("click", function(event) {
-  if (!event.target.matches("#contents-menu-item")) {
-    contentsMenu.classList.remove("show");
-  }
-});
-
 
 const contentsLink = document.querySelector('.contents-link');
 let menuOpen = false;
@@ -87,12 +60,6 @@ contentsLink.addEventListener('click', e => {
   }
 });
 
-document.addEventListener('click', e => {
-  if (!e.target.classList.contains('contents-link') && menuOpen) {
-    contentsLink.removeChild(contentsLink.lastElementChild);
-    menuOpen = false;
-  }
-});
 document.querySelector(".contents-link").addEventListener("click", function(e) {
   e.preventDefault();
   document.querySelector(".contents-menu").classList.toggle("show");
